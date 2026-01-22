8cfac709fe3b4183a9b7fa3408194cbbae82a2f7
Papalitas09 <miaw.raja@gmail.com>
2026-01-22 13:19:59 +0700

    konz

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 });
-// Jalur Khusus Member (Sudah Login)
+// Jalur Khusus Member (Sudah Login)s
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     // Dashboard Admin
